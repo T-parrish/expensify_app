c14b432f42623b6b165a3f0df3846598bbbcbfc7
Taylor Parrish <twpearish@gmail.com>
2018-04-17 15:56:24 +0900

    configured webpack, tests with react-router

--- src/app.js
+++ src/app.js
@@ -1,10 +1,46 @@
 // Application entry point
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom'
 // Wipes slate for browser stylings
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 console.log('app.js is running');
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
+const ExpenseDashboard = () => (
+	<div>
+		<p>This is from my Dashboard component</p>
+	</div>
+);
+
+const AddExpensePage = () => (
+	<div>
+		<p>This is from my AddExpensePage component</p>
+	</div>
+);
+
+const HelpPage = () => (
+	<div>
+		<p>This is from my HelpPage component</p>
+	</div>
+);
+
+const EditExpense = () => (
+	<div>
+		<p>This is from my EditExpense component</p>
+	</div>
+);
+
+const routes = (
+    <BrowserRouter>
+		<div>
+			<Route path="/" component={ExpenseDashboard} exact={true} />
+			<Route path="/create" component={AddExpensePage} exact={true} />
+			<Route path="/help" component={HelpPage} exact={true} />
+			<Route path="/edit" component={EditExpense} exact={true} />
+		</div>
+    </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -25,5 +25,6 @@ module.exports = {
     devtool: 'cheap-module-eval-source-map',
     devServer: {
         contentBase: path.join(__dirname, 'public'),
+        historyApiFallback: true,
     }
 };
