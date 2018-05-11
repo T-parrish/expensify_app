62e23ca1afe1c17a79341e9a9dae78290d444a2b
Taylor Parrish <twpearish@gmail.com>
2018-05-11 10:47:46 +0900

    configuring env for heroku and firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,12 @@ import * as firebase from 'firebase';
 
 // Initialize Firebase
 const config = {
-    apiKey: "AIzaSyBj4DTlLGc8_qSvqRhnSs-ToHeAWrG5fTs",
-    authDomain: "expensify-74a03.firebaseapp.com",
-    databaseURL: "https://expensify-74a03.firebaseio.com",
-    projectId: "expensify-74a03",
-    storageBucket: "expensify-74a03.appspot.com",
-    messagingSenderId: "316006140565"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -4,3 +4,5 @@ import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({
     adapter: new Adapter()
 });
+
+require('dotenv').config({ path: '.env.test' });
